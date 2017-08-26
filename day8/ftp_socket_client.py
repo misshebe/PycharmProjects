1b427a847408b18ebfde57034b88e60c47076434
misshebe <953651508@qq.com>
2017-08-26 11:30:24 +0800

    day8

---
+++ day8/ftp_socket_client.py
@@ -0,0 +1,27 @@
+import socket
+
+client = socket.socket()
+client.connect(('localhost',6969))
+
+while True:
+    cmd = input(">>:").strip()
+    if len(cmd) == 0: continue
+    if cmd.startswith("get"):
+        client.send(cmd.encode())
+        server_response = client.recv(1024)
+        print("server response:",server_response)
+        client.send(b"ready to recv file")
+        file_total_size = int(server_response.decode())
+        received_size = 0
+        filename = cmd.split()[1]
+        f = open(filename + ".new","wb")
+        while received_size < file_total_size:
+            data = client.recv(1024)
+            received_size += len(data)
+            f.write(data)
+            print(file_total_size,received_size)
+        else:
+            print("file recv done",received_size,file_total_size)
+            f.close()
+
+client.close()
